7b92117e1e8de7ed3f63a94d279cc84c6607b71a
Callum Evans <callum.evans@sorted.com>
2018-05-22 12:19:41 +0100

    Some fancier tests

---
+++ Main/Bar.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Bar
+    {
+        private readonly Foo fooService;
+        
+        public Bar(Foo fooService)
+        {
+            this.fooService = fooService;
+        }
+
+        public async Task<string> SomeAsyncStuff()
+        {
+            await Task.Delay(1200);
+            return "I am a string!";
+        }
+
+        public int DoSomethingWithFoo()
+        {
+            return fooService.DoSomeIo();
+        }
+    }
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Threading.Tasks;
 using Main;
 using Xunit;
 using Pose;
@@ -8,21 +8,68 @@ namespace Tests
     public class UnitTest1
     {        
         Shim fooShim = Shim.Replace(() => Is.A<Foo>().DoSomeIo()).With(
-            delegate (Foo @this) { return 99; });
+            (Foo @this) => 99);
+
+        private Shim barShim = Shim.Replace(() => Is.A<Bar>().SomeAsyncStuff()).With(
+            (Bar @this) => Task.FromResult("lol I'm fake"));
+        
+        private Foo foo;
+        private Bar bar;
+
+        public UnitTest1()
+        {
+            foo = new Foo();
+            bar = new Bar(foo);
+        }
         
         [Fact]
         public void Test1()
         {
+            // Arrange
             int? result = null;
-            var classToTest = new Foo();
 
+            // Act
             PoseContext.Isolate(() =>
             {                
-                result = classToTest.DoSomeIo();
+                result = foo.DoSomeIo();
             }, fooShim);
             
             // Assert
             Assert.Equal(99, result);
         }
+
+        [Fact]
+        public void OtherTest()
+        {
+            // Arrange
+            string result = null;
+            
+            // Act
+            PoseContext.Isolate(() =>
+            {
+                result = bar.SomeAsyncStuff().Result;
+            }, barShim);
+            
+            // Assert
+            Assert.Equal("lol I'm fake", result);
+        }
+        
+        [Fact]
+        public void TestBarFooMethod()
+        {
+            // Uses concerete 'Bar' class but it calls the previously shimmed 'Foo' class.
+            
+            // Arrange
+            int? result = null;
+            
+            // Act
+            PoseContext.Isolate(() =>
+            {
+                result = bar.DoSomethingWithFoo();
+            }, fooShim);
+            
+            // Assert
+            Assert.Equal(99, result);
+        }        
     }
 }
